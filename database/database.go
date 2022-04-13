e8b01e26271820f2c9301a6ee95c3b0e26c8e119
klaudia.bartkowska <klaudiabarti@gmailcom>
2022-04-13 16:23:12 +0100

    create db table

---
+++ database/database.go
@@ -0,0 +1,21 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	// to open we need to provide (driverName:, dataSourceName:)
+
+	db, err := sql.Open("sqlite3", "userdata.db")
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	statement, _ := db.Prepare("CREATE TABLE IF NOT EXISTS people(id INTEGER PRIMARY KEY, username TEXT, email TEXT, password TEXT)")
+	statement.Exec()
+
+}
